6966350af9fda38ec78fddae9f64ffd1c3c1520e
Guilherme Alves <gui.alvesdouglas@gmail.com>
2023-08-22 14:44:18 -0300

    Second Commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,10 +1,10 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk'
 
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 20 }),
+  name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().optional(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional()
@@ -12,14 +12,14 @@ const User = g.model('User', {
 
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
-  description: g.string(),
+  description: g.string(), 
   image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
 })
 
 export default config({
-  schema: g 
+  schema: g,
 })
